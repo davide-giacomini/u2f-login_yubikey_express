87cc07240554d2150824768b6f9dc4c6da3a6a4f
Davide Giacomini <giacomini.davide@outlook.com>
2021-11-26 22:48:33 -0600

    Added Vue template

---
+++ client/src/components/U2FComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <button v-on:click="register()">Start registration</button>
+    <button v-on:click="login()">Start login</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'U2FComponent',
+    methods: {
+      register() {},
+      login() {}
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
